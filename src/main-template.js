4117e6acf1d5a11a36cd4f3ed353a3cae0d7a773
Zexi <zexi@zexi.me>
2020-08-18 02:39:57 +0800

    :zap:

--- src/main-template.js
+++ src/main-template.js
@@ -1,12 +1,12 @@
 // 自动做题
-(async function () {
+(function () {
   solve(JSON.parse('{{questions}}'))
 
   function solve (questions) {
     let count = 0
     $('.ep_topic').each((_, el) => {
-      const id = parseInt($(el).find('.ep_radio').attr('name'))
       const radios = $(el).find('.ep_radio')
+      const id = parseInt(radios.attr('name'))
 
       const question = questions.find((x) => x.id === id)
       if (!question) return console.log(`题库缺少：${id}`)
